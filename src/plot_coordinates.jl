644fd373c442be901fe343c7c5bf12f91b6a5683
Alberto Mengali <disberd@gmail.com>
2025-03-08 09:31:15 +0100

    refactor extract_plot_coords

--- src/plot_coordinates.jl
+++ src/plot_coordinates.jl
@@ -1,40 +1,45 @@
+"""
+    INSERT_NAN
+
+ScopedValue use to control whether to insert NaN before each ring within `extract_plot_coords!` unless we are in the first ring being inserted.
+
+Defaults to `true`
+"""
+const INSERT_NAN = Base.ScopedValues.ScopedValue{Bool}(true)
+
 # Extracting lat/lon coordaintes of the borders
-function extract_plot_coords(ll::LATLON)
-    (;lat, lon) = ll
-    out = map(Float32 ∘ ustrip, (;lat, lon))
+function extract_plot_coords!(lat::Vector{T}, lon::Vector{T}, ll::LATLON) where T <: Number
+    f(x) = convert(T, ustrip(x))
+    push!(lat, f(ll.lat))
+    push!(lon, f(ll.lon))
+    return nothing
 end
-function extract_plot_coords(c::CART)
-    lat = c.y
-    lon = c.x
-    out = map(Float32 ∘ ustrip, (;lat, lon))
+function extract_plot_coords!(lat::Vector{T}, lon::Vector{T}, c::CART) where T <: Number
+    f(x) = convert(T, ustrip(x))
+    push!(lat, f(c.y))
+    push!(lon, f(c.x))
+    return nothing
 end
-extract_plot_coords(p::VALID_POINT) = extract_plot_coords(coords(p))
-function extract_plot_coords(v::Vector)
-    nelem = length(v)
-    lat = Vector{Float32}(undef, nelem)
-    lon = Vector{Float32}(undef, nelem)
-    for i in eachindex(v, lat, lon)
-        c = extract_plot_coords(v[i])
-        lat[i] = c.lat
-        lon[i] = c.lon
+extract_plot_coords!(lat, lon, p::VALID_POINT) = extract_plot_coords!(lat, lon, coords(p))
+
+function extract_plot_coords!(lat, lon, els::Vector)
+    for el in els
+        extract_plot_coords!(lat, lon, el)
     end
-    return (;lat, lon)
+    return nothing
 end
 
-function extract_plot_coords(ring::VALID_RING)
-    nelem = nvertices(ring)
-    lat = Vector{Float32}(undef, nelem)
-    lon = Vector{Float32}(undef, nelem)
-    v = vertices(ring)
-    for i in eachindex(v, lat, lon)
-        c = extract_plot_coords(v[i])
-        lat[i] = c.lat
-        lon[i] = c.lon
+function extract_plot_coords!(lat, lon, ring::VALID_RING)
+    if INSERT_NAN[] && !isempty(lat) && !isempty(lon)
+        extract_plot_coords!(lat, lon, LatLon(NaN, NaN))
+    end
+    # Insert the coordinates of each point of the ring
+    for v in vertices(ring)
+        extract_plot_coords!(lat, lon, v)
     end
     # We add the first point to the end of the array to close the ring
-    push!(lat, first(lat))
-    push!(lon, first(lon))
-    return (;lat, lon)
+    extract_plot_coords!(lat, lon, first(vertices(ring)))
+    return nothing
 end
 
 geom_iterable(pa::Union{Multi, PolyArea}) = rings(pa)
@@ -51,19 +56,26 @@ and `lon` contain the concateneated lat/lon values of each ring separated by
 `NaN32` values. This is done to allow plotting multiple separated borders in a
 single trace.
 """
-function extract_plot_coords(inp::Union{Multi, PolyArea, RegionBorders})
+function extract_plot_coords!(lat, lon, inp)
+    applicable(geom_iterable, inp) || throw(ArgumentError("last input of `extract_plot_coords!` must implement the `geom_iterable` function to use the generic fallback. Alternatively, a specific method for `extract_plot_coords!(lat, lon, inp)` must be implemented for type $(typeof(inp))."))
     iterable = geom_iterable(inp)
-    length(iterable) == 1 && return extract_plot_coords(first(iterable))
-	lon = Float32[]
-	lat = Float32[]
 	for geom ∈ iterable
-		c = extract_plot_coords(geom)
-        append!(lat, c.lat)
-        append!(lon, c.lon)
-        if geom !== last(iterable)
-            push!(lat, NaN32)
-            push!(lon, NaN32)
-        end
+        extract_plot_coords!(lat, lon, geom)
 	end
-	(;lat, lon)
+    return nothing
 end
+
+"""
+    extract_plot_coords([T::Type{<:AbstractFloat} = Float32], item)
+
+Returns a NamedTuple with two vectors `lat` and `lon` containing the corresponding coordinates of all the points contained within the provided `item`.
+
+This is mostly intended to simplify creation of the `lat` and `lon` keyword arguments to provide to the `scattergeo` function from `PlotlyBase`. By default points are converted to Float32 and NaN values are inserted between each `Ring` to allow plotting multiple polyareas within a single trace.
+"""
+function extract_plot_coords(T::Type{<:AbstractFloat}, item)
+    lat = T[]
+    lon = T[]
+    extract_plot_coords!(lat, lon, item)
+    return (; lat, lon)
+end
+extract_plot_coords(item) = extract_plot_coords(Float32, item)
