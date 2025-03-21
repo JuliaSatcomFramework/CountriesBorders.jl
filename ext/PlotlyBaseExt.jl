83614b1c5bbbac051b3f845d7cf278632b11561a
Alberto Mengali <disberd@gmail.com>
2025-03-21 16:54:50 +0100

    remove piracy on scattergeo

--- ext/PlotlyBaseExt.jl
+++ ext/PlotlyBaseExt.jl
@@ -3,14 +3,8 @@ using PlotlyBase
 using CountriesBorders: Multi, Domain, PolyArea, extract_plot_coords, LatLon, 🌐, Point, RegionBorders
 
 function PlotlyBase.scattergeo(p::RegionBorders; kwargs...)
-	(;lon, lat) = extract_plot_coords(p)
+    (;lon, lat) = extract_plot_coords(p)
 	scattergeo(; lat, lon, mode="lines", kwargs...)
-end
-
-# This is type piracy so it should be removed probably
-function PlotlyBase.scattergeo(ps::Vector{<:Union{LatLon, Point{🌐, <:LatLon}}}; kwargs...)
-	(;lon, lat) = extract_plot_coords(ps)
-	scattergeo(; lat, lon, kwargs...)
 end
 
 end
