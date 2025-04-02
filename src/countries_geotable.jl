82fe0170ade7104c8b9002eb543d6856401e2821
Alberto Mengali <disberd@gmail.com>
2025-04-02 15:26:05 +0200

    remove internal geotable fun tions

--- src/countries_geotable.jl
+++ src/countries_geotable.jl
@@ -1,11 +1,5 @@
 const GEOTABLE_DICT = Dict{Int, GeoTables.GeoTable}()
 
-function set_geotable!(geotable::GeoTables.GeoTable, resolution::Int)
-    GEOTABLE_DICT[resolution] = geotable
-end
-get_default_geotable_resolution() =
-    get_geotable(; resolution = nothing)
-
 function get_geotable(; resolution = nothing, force = false, kwargs...)
     resolution = check_resolution(resolution; force)
     force && haskey(GEOTABLE_DICT, resolution) && delete!(GEOTABLE_DICT, resolution)
